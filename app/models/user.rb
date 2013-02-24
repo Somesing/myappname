772d2202e7bd8878592fbbb4fe44eee9bd30cfce
Somesing <darrenyin0110@yahoo.com>
2013-02-24 16:36:02 -0500

    what is going on

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,4 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me
   attr_accessible :clearance, :name
 
-  validates :name, :length => { 
-
-  	:minimum => 2, 
-  	:too_short => "Must have at least %{count} words"
-  
-  }
-
 end
